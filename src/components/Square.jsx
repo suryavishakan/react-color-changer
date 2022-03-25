64fe6d2d6b9d76799323760e4a62a50682e1d259
surya vishakan <suryavishakan050@gmail.com>
2022-03-25 17:14:32 +0530

    created the color changer app

---
+++ src/components/Square.jsx
@@ -0,0 +1,26 @@
+import PropTypes from "prop-types";
+
+const Square = ({ colorValue, hexValue, isDarkText }) => {
+  return (
+    <section
+      className="square"
+      style={{
+        backgroundColor: colorValue,
+        color: isDarkText ? "#fff" : "#000",
+      }}
+    >
+      <p>{colorValue ? colorValue : "Empty value"}</p>
+      <p>{hexValue ? hexValue : null}</p>
+    </section>
+  );
+};
+
+Square.defaultProps = {
+  colorValue: "Empty color value",
+};
+
+Square.propTypes = {
+  colorValue: PropTypes.string.isRequired,
+};
+
+export default Square;
